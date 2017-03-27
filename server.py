db836887e7aef3a27467077f48411c9a64d6a83f
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-03-27 16:39:20 -0700

    Created server.py file and homepage.html template

---
+++ server.py
@@ -0,0 +1,37 @@
+"""Run Planner"""
+
+from jinja2 import StrictUndefined
+from flask import Flask, jsonify, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from model import connect_to_db, db, Runner, Plan, Run
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+
+    
+    app.run(port=5050, host='0.0.0.0')
